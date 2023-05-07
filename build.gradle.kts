6bb7fb7da0ced8e386e96ed17a08897a9e3d8454
ITesserakt <potryas85@mail.ru>
2023-05-08 00:00:40 +0300

    Init gradle project

---
+++ build.gradle.kts
@@ -0,0 +1,33 @@
+fun DependencyHandlerScope.corba(dependencyNotation: String) =
+    add("corba", dependencyNotation)
+
+plugins {
+    id("java")
+}
+
+group = "org.tesserakt"
+version = "1.0-SNAPSHOT"
+
+configurations {
+    create("corba")
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.9.1"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    corba("org.jacorb:jacorb-idl-compiler:3.6.1")
+    corba("org.jacorb:jacorb-omgapi:3.6.1")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+task<JavaExec>("buildCorba") {
+    mainClass.set("org.jacorb.idl.parser")
+    classpath = configurations.getByName("corba")
+}
